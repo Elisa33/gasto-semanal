6b33adab6978ed2723ceb99eeed156c7bcbcef0a
elisa33 <pipi.tdfuego@gmail.com>
2021-01-13 11:49:11 -0400

    create class presupuesto

---
+++ js/app.js
@@ -0,0 +1,42 @@
+// variables y selectores
+const formulario = document.querySelector('#agregar-gasto');
+const gastoListado = document.querySelector('#gastos ul');
+
+// eventos
+eventListeners();
+
+function eventListeners() {
+	document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
+}
+// classes
+
+class Presupuesto {
+	constructor(presupuesto) {
+		this.presupuesto = Number(presupuesto);
+		this.restante = Number(presupuesto);
+		this.gastos = [];
+	}
+}
+
+class UI {}
+
+const ui = new UI();
+
+let presupuesto;
+
+// funciones
+
+function preguntarPresupuesto() {
+	const presupuestoUsuario = prompt('Escribe tu presupuesto');
+	if (
+		presupuestoUsuario === '' ||
+		presupuestoUsuario === null ||
+		isNaN(presupuestoUsuario) ||
+		presupuestoUsuario <= 0
+	) {
+		window.location.reload();
+	}
+	//presupuesto valido
+	presupuesto = new Presupuesto(presupuestoUsuario);
+	console.log(presupuesto);
+}
